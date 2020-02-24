4d07d61fb7e97cc460b4bf59c6ec526bbdbde44c
Florian <floriandupuis1996@gmail.com>
2020-02-24 23:37:04 +0100

    Corrections Layout pour déploiement

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import Project from '../components/project';
 
 export const query = graphql`
